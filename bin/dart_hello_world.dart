d4ddb8f924c7e957d4e25a2d8acd34e9df623cec
gustavo <gustavo96ma@gmail.com>
2025-02-05 20:42:03 -0300

    first commit

---
+++ bin/dart_hello_world.dart
@@ -0,0 +1,15 @@
+void main(){
+
+  String texto = 'doideira total';
+  int quantidade = 10;
+  double divisao = 1/3;
+  bool booleano = true;
+  List<String> lista = ['a', 'b', 'c', 'd', 'e', 'f'];
+  Map<String, int> map = {'chave': 2, 'chave2': 4};
+
+  print(lista.runtimeType);
+
+
+  
+}
+
