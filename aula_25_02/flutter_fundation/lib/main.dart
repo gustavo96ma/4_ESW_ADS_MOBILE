bbd89460f1a33575660904b26e04398ef0c101ac
gustavo96ma <gustavo96ma@gmail.com>
2025-02-25 22:18:59 -0300

    aula 25/02

---
+++ aula_25_02/flutter_fundation/lib/main.dart
@@ -0,0 +1,75 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MainApp());
+}
+
+class MainApp extends StatefulWidget {
+  const MainApp({super.key});
+
+  @override
+  State<MainApp> createState() => _MainAppState();
+}
+
+class _MainAppState extends State<MainApp> {
+  List<Tarefa> listaAfazeres = [
+    Tarefa(
+      nome: 'Tarefa 1',
+      descricao: 'Descrição da tarefa 1',
+      concluido: false,
+    )
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.blue,
+          title: Text('Lista de afazeres'),
+        ),
+        body: Column(
+          children: [
+            ListView.builder(
+              itemCount: listaAfazeres.length,
+              itemBuilder: (context, index) {
+                return CheckboxListTile(
+                  onChanged: (newValue) {
+                    if (newValue == null) {
+                      listaAfazeres[index].concluido = false;
+                      return;
+                    }
+                    setState(() {
+                      listaAfazeres[index].concluido = newValue;
+                    });
+                  },
+                  value: listaAfazeres[index].concluido,
+                  title: Text(listaAfazeres[index].nome),
+                  subtitle: Text(listaAfazeres[index].descricao),
+                );
+              },
+            ),
+            Row(
+              children: [
+                TextField(),
+                IconButton(onPressed: () {}, icon: Icon(Icons.send)),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class Tarefa {
+  String nome;
+  String descricao;
+  bool concluido;
+
+  Tarefa({
+    required this.nome,
+    required this.descricao,
+    required this.concluido,
+  });
+}
