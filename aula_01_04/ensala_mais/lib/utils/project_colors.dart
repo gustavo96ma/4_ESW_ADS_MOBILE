22f72ee23ec581f7705399a87aaed2e323619187
gustavo96ma <gustavo96ma@gmail.com>
2025-04-01 22:14:29 -0300

    01/04

---
+++ aula_01_04/ensala_mais/lib/utils/project_colors.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class ProjectColors {
+  Color mainGreen = Color(0xff235319);
+  Color backgroundColor = Color(0xFFa4bc98);
+  Color cardColor = Color(0xFFf4f6ec);
+  Color buttonTextColor = Color(0xFF50713c);
+  Color navbarTextHoverColor = Color(0xffF1E0C5);
+  Color navbarTextColor = Colors.white;
+  Color errorButtonForegroundColor = Color.fromARGB(255, 204, 225, 192);
+}
